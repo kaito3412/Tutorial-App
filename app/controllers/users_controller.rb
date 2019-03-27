1f96d142e70e4fbfaa344c74ebf18c7e0a879a1c
kaitomorizumi <k.morizumi.p@gmail.com>
2019-03-27 12:16:21 +0000

    basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "ユーザーの新規作成に成功しました。"
       redirect_to @user
     else
@@ -26,4 +27,3 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 end
-   
