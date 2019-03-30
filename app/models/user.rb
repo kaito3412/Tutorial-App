726ea903e253956c4ced3cac9c88a64c039af7ca
kaitomorizumi <k.morizumi.p@gmail.com>
2019-03-30 14:20:12 +0000

    Finish user edit index destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
+
+
+
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
@@ -5,6 +8,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-                    has_secure_password
-                    validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
---
+++ db/migrate/20190330140553_add_admin_to_users.rb
@@ -0,0 +1,8 @@
+class AddAdminToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :admin, :boolean, default: false
+  end
+end
+
+
+
