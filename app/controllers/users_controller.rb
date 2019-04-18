14cffaa4acd9d3eb55384b25fa3472d924263666
kaitomorizumi <k.morizumi.p@gmail.com>
2019-04-18 07:46:34 +0000

    Finish Attendances edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,54 +8,49 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
   
+  
 
-def show
-  @user = User.find(params[:id])
-  if params[:first_day].nil?
-    @first_day = Date.today.beginning_of_month
-  else
-    @first_day = Date.parse(params[:first_day])
-  end
-  @last_day = @first_day.end_of_month  
-  (@first_day..@last_day).each do |day|
-    unless @user.attendances.any? {|attendance| attendance.worked_on == day}
-      record = @user.attendances.build(worked_on: day)
-      record.save
-    
+  def show
+    @user = User.find(params[:id])
+    @first_day = first_day(params[:first_day])
+    @last_day = @first_day.end_of_month
+    (@first_day..@last_day).each do |day|
+      unless @user.attendances.any? {|attendance| attendance.worked_on == day}
+        record = @user.attendances.build(worked_on: day)
+        record.save
+      end
     end
+    @dates = user_attendances_month_date
+    @worked_sum = @dates.where.not(started_at: nil).count
   end
-  @dates = @user.attendances.where('worked_on >= ? and worked_on <= ?', @first_day, @last_day).order('worked_on')
-  @worked_sum = @dates.where.not(started_at: nil).count
-end
 
   
   
+    
   
-  
-  def user_params
-      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
-  end
+
   
   def edit_basic_info
-  @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
   
-def update_basic_info
-  @user = User.find(params[:id])
-  if @user.update_attributes(basic_info_params)
+  def update_basic_info
+    @user = User.find(params[:id])
+    if @user.update_attributes(basic_info_params)
     flash[:success] = "基本情報を更新しました。"
     redirect_to @user   
   else
     render 'edit_basic_info'
+    end
   end
-end 
-end
+   
+
   
-    def destroy
+  def destroy
     User.find(params[:id]).destroy
     flash[:success] = "削除しました。"
     redirect_to users_url
-    end
+  end
 
   def new
     @user = User.new
@@ -85,32 +80,35 @@ end
   end
 
   private
-
+    
   def user_params
     params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
   end
+
   
-    def basic_info_params
+  def basic_info_params
     params.require(:user).permit(:basic_time, :work_time)
-    end
+  end
   
 
     # beforeアクション
 
     # ログイン済みユーザーか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
     end
+  end
 
     # 正しいユーザーかどうか確認
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+    
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+
 Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/signup',   to: 'users#new'
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   get '/edit-basic-info/:id', to: 'users#edit_basic_info', as: :basic_info
   patch 'update-basic-info',  to: 'users#update_basic_info'
+  get 'users/:id/attendances/:date/edit', to: 'attendances#edit', as: :edit_attendances
+  patch 'users/:id/attendances/:date/update', to: 'attendances#update', as: :update_attendances
   resources :users do
     resources :attendances, only: :create
   end
